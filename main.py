4e0ca142b96bf3ec766f4e1c0a86cd783351b750
SJKL612 <143746119+SJKL612@users.noreply.github.com>
2023-10-23 20:16:08 -0600

    Update main.py
    updated feelings' if-elif-else statements to capture more conditions for mental health tracking

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ feelings = 1
 while feelings in range(1,11):
     feelings = int(input())
 
-if feelings < 5:
+if feelings < 3:
     print("What happened? Would you like to talk about it?")
-else:
+elif 3 <= feelings <= 8:
     print("Awesome! What's something interesting that happened today?")
+else: 
+    print("Fantstic! Write 3 things that you are grateful today=> ")
 
-    #hi
+# Guten Tag!
+# It's new. 
