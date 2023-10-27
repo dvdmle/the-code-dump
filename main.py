e8322d2ff07b236b6e5fc3c6ccc84d2700ec6e58
dvdmle <dcbllrd@proton.me>
2023-10-26 18:30:41 -0600

    migrate feelings tracker to function

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def feelingsTracker():
+    print("On a scale of 1 to 10, how are you feeling today?")
+
+    feelings = 0
+
+    while not feelings in range(1,11):
+        feelings = int(input())
+
+    if feelings < 3:
+        print("What happened? Would you like to talk about it?")
+    elif 3 <= feelings <= 8:
+        print("Awesome! What's something interesting that happened today?")
+    else: 
+        print("Fantstic! Write 3 things that you are grateful today=> ")
+
 def sleepWellness():
     sleep_hours = int(input("How many hours of sleep did you get today? "))
 
@@ -20,19 +35,6 @@ def exit():
 
 print("Welcome to the mental health app!")
 print()
-print("On a scale of 1 to 10, how are you feeling today?")
-
-feelings = 0
-
-while not feelings in range(1,11):
-    feelings = int(input())
-
-if feelings < 3:
-    print("What happened? Would you like to talk about it?")
-elif 3 <= feelings <= 8:
-    print("Awesome! What's something interesting that happened today?")
-else: 
-    print("Fantstic! Write 3 things that you are grateful today=> ")
 
 # Guten Tag!
 # It's new. 
